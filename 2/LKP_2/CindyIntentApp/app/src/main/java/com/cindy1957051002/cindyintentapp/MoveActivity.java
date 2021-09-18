ea651143601621095828ae66a03255fe522015f9
Cindylibra28 <cindylibra2234@gmail.com>
2021-09-18 20:03:09 +0700

    commit 1

---
+++ 2/LKP_2/CindyIntentApp/app/src/main/java/com/cindy1957051002/cindyintentapp/MoveActivity.java
@@ -0,0 +1,14 @@
+package com.cindy1957051002.cindyintentapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MoveActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_move);
+    }
+}
